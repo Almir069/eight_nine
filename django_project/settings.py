02a3b224b4c14d80433ee2e29c826c94bd4f68f2
Альмир Муратов <muratovalmir456@gmail.com>
2024-10-21 07:49:32 +0600

    chapter twelve and thirteen and fourteen

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
+    "articles.apps.ArticlesConfig",
 ]
 
 MIDDLEWARE = [
@@ -98,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "America/New_York"
 
 USE_I18N = True
 
